df714ad17f663238d5f136c00cb45b1f081bf72a
msjulietruong <msjulietruong@gmail.com>
2024-04-07 00:39:58 -0700

    Revised to only include short and sweet quotes!

--- quotes.js
+++ quotes.js
@@ -12,41 +12,6 @@ const quotes = [
         author: "Eiichiro Oda"
     },
     {
-        quote: `I saw my life branching out before me like the green fig tree in the story.
-         From the tip of every branch, like a fat purple fig, a wonderful future beckoned 
-         and winked. One fig was a husband and a happy home and children, and another fig 
-         was a famous poet and another fig was a brilliant professor, and another fig was 
-         Ee Gee, the amazing editor, and another fig was Europe and Africa and South Ameri
-         -ca, and another fig was Constantin and Socrates and Attila and a pack of other 
-         lovers with queer names and offbeat professions, and another fig was an Olympic 
-         lady crew champion, and beyond and above these figs were many more figs I couldn't
-          quite make out. I saw myself sitting in the crotch of this fig tree, starving to 
-          death, just because I couldn't make up my mind which of the figs I would choose.
-           I wanted each and every one of them, but choosing one meant losing all the rest,
-            and, as I sat there, unable to decide, the figs began to wrinkle and go black,
-             and, one by one, they plopped to the ground at my feet.`,
-        author: "Sylvia Plath"
-    },
-    {
-        quote: `It is easy to mourn the lives we aren't living. Easy to wish we'd developed
-                other other talents, said yes to different offers. Easy to wish we'd worked 
-                harder, loved better, handled our finances more astutely, been more popular,
-                stayed in the band, gone to Australia, said yes to the coffee or done more 
-                bloody yoga. It takes no effort to miss the friends we didn't make and the
-                work we didn't do, the people we didn't do and the people we didn't marry
-                and the children we didn't have. It is not difficult to see yourself thr
-                -ough the lens of other people, and to wish you were all the different 
-                kaleidoscopic versions of you they wanted you to be. It is easy to regret,
-                and keep regretting, ad infinitum, until our time runs out. But it is not
-                lives we regret not living that are the real problem. It is the regret it
-                -self. It's the regret that makes us shrivel and wither and feel like our
-                own and other people's worst enemy. We can't tell if any of those other
-                versions would have been better or worse. Those lives are happening,
-                it is true, but you are happening as well, and that is the happening
-                we have to focus on.`,
-        author: "Matt Haig"
-},
-    {
         quote: `The only way to learn is to live.`,
         author: "Matt Haig"
 
