a16d9f4a2c1552cbf96b27a3cd6b98259ca104b9
msjulietruong <msjulietruong@gmail.com>
2024-04-07 00:21:00 -0700

    Created a quotes file to contain quotes from some of my favorite books!

---
+++ quotes.js
@@ -0,0 +1,86 @@
+const quotes = [
+    {
+        quote: "Don't forget to smile in any situation. As long as you are alive, there will be better things later, and there will be many.",
+        author: "Eiichiro Oda"
+    },
+    {
+        quote: "Those who stand at the top determine what's wrong and what's right! This very place is neutral ground! Justice will prevail, you say? But of course it will! Whoever wins this war becomes justice!",
+        author: "Eiichiro Oda"
+    },
+    {
+        quote: "You can't get back what you've lost, what's important now is what it is that you still have.",
+        author: "Eiichiro Oda"
+    },
+    {
+        quote: `I saw my life branching out before me like the green fig tree in the story.
+         From the tip of every branch, like a fat purple fig, a wonderful future beckoned 
+         and winked. One fig was a husband and a happy home and children, and another fig 
+         was a famous poet and another fig was a brilliant professor, and another fig was 
+         Ee Gee, the amazing editor, and another fig was Europe and Africa and South Ameri
+         -ca, and another fig was Constantin and Socrates and Attila and a pack of other 
+         lovers with queer names and offbeat professions, and another fig was an Olympic 
+         lady crew champion, and beyond and above these figs were many more figs I couldn't
+          quite make out. I saw myself sitting in the crotch of this fig tree, starving to 
+          death, just because I couldn't make up my mind which of the figs I would choose.
+           I wanted each and every one of them, but choosing one meant losing all the rest,
+            and, as I sat there, unable to decide, the figs began to wrinkle and go black,
+             and, one by one, they plopped to the ground at my feet.`,
+        author: "Sylvia Plath"
+    },
+    {
+        quote: `It is easy to mourn the lives we aren't living. Easy to wish we'd developed
+                other other talents, said yes to different offers. Easy to wish we'd worked 
+                harder, loved better, handled our finances more astutely, been more popular,
+                stayed in the band, gone to Australia, said yes to the coffee or done more 
+                bloody yoga. It takes no effort to miss the friends we didn't make and the
+                work we didn't do, the people we didn't do and the people we didn't marry
+                and the children we didn't have. It is not difficult to see yourself thr
+                -ough the lens of other people, and to wish you were all the different 
+                kaleidoscopic versions of you they wanted you to be. It is easy to regret,
+                and keep regretting, ad infinitum, until our time runs out. But it is not
+                lives we regret not living that are the real problem. It is the regret it
+                -self. It's the regret that makes us shrivel and wither and feel like our
+                own and other people's worst enemy. We can't tell if any of those other
+                versions would have been better or worse. Those lives are happening,
+                it is true, but you are happening as well, and that is the happening
+                we have to focus on.`,
+        author: "Matt Haig"
+},
+    {
+        quote: `The only way to learn is to live.`,
+        author: "Matt Haig"
+
+    },
+    {
+        quote: `Stuff your eyes with wonder, he said, live as if you'd drop dead in ten seconds. See the world. It's more fantastic than any dream made or paid for in factories.`,
+        author: "Ray Bradbury"
+    },
+    {
+        quote: `We may not get to choose how we die, but we can choose how we live.
+    The universe may forget us, but it doesn't matter. Because we are the ants, and we'll keep marching on.`,
+        author: "Shaun David Hutcherson"
+
+},
+    {
+        quote: `What's the point of having a voice if you're gonna be silent in those moments you shouldn't be?`,
+        author: "Angie Thomas"
+    },
+    {
+        quote: `At an early age I learned that people make mistakes, and you have to decide if their mistakes are bigger than your love for them.`,
+        author: "Angie Thomas"
+    },
+    {
+        quote: `The truth behind one’s charm is kindness. Become a good person, is all.`,
+        author: "ONE"
+    },
+    {
+        quote: `We accept the love we think we deserve.`,
+        author: "Stephen Chbosky"
+    },
+    {
+        quote: "This moment will just be another story someday.",
+        author: "Stephen Chbosky"
+    }
+];
+
+export default quotes;
